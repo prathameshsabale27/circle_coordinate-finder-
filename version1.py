b52fb6facfd9d126ec3c2284ac8af28fe5cdf294
PRATHAMESH <105699151+prathameshsabale27@users.noreply.github.com>
2026-03-26 22:34:14 +0530

    Add value_printer function to calculate coordinates
    Implement value_printer function to calculate and print x, y coordinates based on radius and sections.

---
+++ version1.py
@@ -0,0 +1,28 @@
+import math
+def value_printer(r,sections):
+    a = r
+    b = 360/sections
+    for i in range(sections):
+     x = round(a*math.sin(i*b))
+     y = round(a*math.cos(i*b))
+     if (x>=0 and y>=0):
+         print ("x = ",x,"y = ", y)
+     elif (x<0 and y<0):
+         print ("x =",x,"y =", y)
+     elif (x<0 and y>=0):
+         print ("x =",x,"y = ", y)
+     else:
+          print ("x = ",x,"y =", y)
+    
+    
+
+    return 
+
+
+print("Enter radius ")
+#r= int(input())
+r=5
+print("Enter sections")
+#sections = int(input())
+sections=12
+value_printer(r,sections)
