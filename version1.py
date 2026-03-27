cdd2ab9042330fbf969e737ce163e072b5633673
PRATHAMESH <105699151+prathameshsabale27@users.noreply.github.com>
2026-03-28 00:35:17 +0530

    Refactor value_printer to improve angle calculation
    Refactor value_printer function for clarity and precision.

--- version1.py
+++ version1.py
@@ -1,26 +1,24 @@
 import math
-def value_printer(r,sections):
-    a = r
-    b = 360/sections
-    for i in range(sections):
-     x = round(a*math.sin(i*b))
-     y = round(a*math.cos(i*b))
-     if (x>=0 and y>=0):
-         print ("x = ",x,"y = ", y)
-     elif (x<0 and y<0):
-         print ("x =",x,"y =", y)
-     elif (x<0 and y>=0):
-         print ("x =",x,"y = ", y)
-     else:
-          print ("x = ",x,"y =", y)
-    
-    
 
-    return 
+def value_printer(r, sections):
+    a = r
+    b = 360 / sections
+    
+    for i in range(sections):
+        angle = math.radians(i * b)  
+        
+        x = round(a * math.sin(angle), 1)
+        y = round(a * math.cos(angle), 1)
+        
+        print("x =", x, "y =", y)
+
+    return
 
 
 print("Enter radius ")
-r= int(input())
+r = int(input())
+
 print("Enter sections")
 sections = int(input())
-value_printer(r,sections)
+
+value_printer(r, sections)
